cd4e906678c7c005affa8fdd135f05df6ced2c33
nomanliaqat48 <noman.liaqat@invozone.com>
2024-03-29 22:00:37 +0500

    Add Back Camera Feature for mobilee

--- src/features/dashboard/scanQRCodeModal.jsx
+++ src/features/dashboard/scanQRCodeModal.jsx
@@ -1,15 +1,16 @@
-import React,{useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import Modal from "react-bootstrap/Modal";
-import QrReader from "react-qr-scanner";
+// import QrReader from "react-qr-scanner";
+import { QrReader } from "react-qr-reader";
 
 const previewStyle = {
   height: 250,
   width: "100%",
 };
 
-export default function ScanQRCodeModal({show, handleClose, setSendAddress}) {
+export default function ScanQRCodeModal({ show, handleClose, setSendAddress }) {
   const handleScan = (data) => {
-    if(!data) return;
+    if (!data) return;
     console.log("data", data);
     setSendAddress(data.text);
     handleClose();
@@ -19,27 +20,29 @@ export default function ScanQRCodeModal({show, handleClose, setSendAddress}) {
     console.error(err);
   };
 
-  const [cameraId, setCameraId] = useState('user');
+  const [cameraId, setCameraId] = useState("user");
   const videoRef = useRef(null);
 
   const handleCameraSwitch = () => {
-    setCameraId(cameraId === 'user' ? 'environment' : 'user');
+    setCameraId(cameraId === "user" ? "environment" : "user");
   };
 
   return (
-    <Modal show={show} onHide={() => handleClose()} dialogClassName="modal-width">
+    <Modal
+      show={show}
+      onHide={() => handleClose()}
+      dialogClassName="modal-width"
+    >
       <Modal.Header closeButton>
         <Modal.Title>Scan QR Code</Modal.Title>
       </Modal.Header>
       <Modal.Body className="mb-5">
-        <QrReader
+        {/* <QrReader
           delay={100}
           cameraId={cameraId}
           style={previewStyle}
           key={cameraId}
-  constraints={{ video: true, facingMode: { exact: cameraId }, }}
-  
-
+          constraints={{ video: true, facingMode: { exact: cameraId } }}
           onError={handleError}
           onScan={handleScan}
           className="mt-5"
@@ -47,9 +50,19 @@ export default function ScanQRCodeModal({show, handleClose, setSendAddress}) {
           //   facingMode: cameraId
           // }}
           // ref={videoRef}
-        />
-        <button onClick={handleCameraSwitch} className="text-center">Switch Camera</button>
-     
+        /> */}
+        <QrReader
+        delay={500}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: '100%' }}
+        facingMode="environment"
+        videoConstraints={{ aspectRatio: 1 }}
+      />
+        <button onClick={handleCameraSwitch} className="text-center">
+          Switch Camera
+        </button>
+
         {/* <p>{this.state.result}</p> */}
       </Modal.Body>
     </Modal>
